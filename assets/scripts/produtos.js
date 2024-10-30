fc6ac90073de6f6017733759af4e450a6258958c
@alissondev <alissonhannemann7@gmail.com>
2024-10-30 10:34:56 -0300

    Update produtos.js

--- assets/scripts/produtos.js
+++ assets/scripts/produtos.js
@@ -5,6 +5,9 @@ let preco1 = document.querySelector('.preco1');
 let imagem1 = document.getElementById('imagemProduto');
 const fundo1 = document.querySelector('.fundo');
 const fechar = document.querySelector('#fechar');
+const contadorQtd = document.getElementById('contador');
+
+import { reset } from "./qtd.js";
 
 produtos.forEach((slide) => {
     slide.addEventListener('click', () => {
@@ -12,25 +15,28 @@ produtos.forEach((slide) => {
         let preco = slide.querySelector('.preco').textContent.trim();
         const imagem = slide.querySelector('.swiper-slide img').getAttribute('src');
 
-        console.log(imagem)
-
         titulo1.textContent = titulo;
         preco1.textContent = preco;
         imagem1.src = imagem;
 
         fundo1.style.display = 'block';
-        telaProdutos.setAttribute('id', 'produtosOpen');
+        telaProdutos.setAttribute('id', 'produtosOn');
     })
 })
 
 fundo1.addEventListener('click', () => {
     fundo1.style.display = 'none';
     telaProdutos.removeAttribute('id', 'produtosOpen');
+    contadorQtd.textContent = '1';
+    reset();
 })
 
 fechar.addEventListener('click', () => {
     fundo1.style.display = 'none';
     telaProdutos.removeAttribute('id', 'produtosOpen');
+    contadorQtd.textContent = '1';
+    reset();
 })
+
 
 
